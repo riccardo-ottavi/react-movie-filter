c42cecf9af083516af9b0efbf97f3d4bdb145837
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-01 14:08:08 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+const filmsList =  [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+console.log(filmsList)  
+
+function App() {
+
+  return (
+    <>
+      <h1>Prova</h1>
+    </>
+  )
+}
+
+export default App
