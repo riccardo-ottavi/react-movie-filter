d8ec52c44d5da9b71612551157a769ee37942286
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-01 14:38:45 +0200

    sposta la logica della lista in un componente separato

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
 import './App.css'
+import FilmsList from './components/FilmsList'
 import MyHeader from './components/MyHeader'
 
-const filmsList =  [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
-  ]
-
-console.log(filmsList)  
 
 function App() {
   return (
     <>
       <MyHeader/>
-      <div className="list">
-        <ul>
-        {filmsList.map(film => 
-          <li
-          //key=
-          >{film.title}</li>
-        )}
-        </ul>
-          <select name="Genere" id="Genere">
-            <option value=""></option>
-            <option value="Fantascienza">Fantascienza</option>
-            <option value="Thriller">Thriller</option>
-            <option value="Romantico">Romantico</option>
-            <option value="Azione">Azione</option>
-          </select>
-      </div>
+      <FilmsList/>
     </>
   )
 }
---
+++ src/components/FilmsList.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const filmsList =  [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+console.log(filmsList)  
+
+export default function FilmsList(){
+
+    const [filteredList, setFilteredList] = useState([]);
+
+    return(
+        <div className="list">
+        <ul>
+        {filmsList.map(film => 
+          <li
+          //key=
+          >{film.title}</li>
+        )}
+        </ul>
+          <select name="Genere" id="Genere">
+            <option value=""></option>
+            <option value="Fantascienza">Fantascienza</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Romantico">Romantico</option>
+            <option value="Azione">Azione</option>
+          </select>
+      </div>
+    )
+}
